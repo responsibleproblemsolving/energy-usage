cacc8706546eea9d90ef46a75c690e172d091196
kadan <klottick@haverford.edu>
2019-07-17 11:32:22 -0400

    added timing function

--- energy-meter/energy-meter.py
+++ energy-meter/energy-meter.py
@@ -1,14 +1,19 @@
 import evaluate
+import time
 
 # FUNCTION TO BE EVALUATED
 def fib(n):
     if (n<=2): return 1
     else: return fib(n-1) + fib(n-2)
 
+def sleep(n):
+    time.sleep(n)
+    return 1
+
 
 
 def main():
-    evaluate.evaluate(fib,35)
+    evaluate.evaluate(sleep, 10)
 
 
 
