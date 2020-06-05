665965fba0c16b12fe25af11b6d94483d8f31ded
Ziyao Wang <ziyaow36@gmail.com>
2020-06-05 11:37:12 -0400

    debugging

--- energyusage/report_test.py
+++ energyusage/report_test.py
@@ -1,17 +1,3 @@
-import time
-import statistics
-from timeit import default_timer as timer
-from multiprocessing import Process, Queue
-import os
-import datetime
-import subprocess
-import queue
+import mlco2_report
 
-
-import energyusage.utils as utils
-import energyusage.convert as convert
-import energyusage.locate as locate
-import energyusage.report as report
-import energyusage.mlco2_report as mlco2_report
-
-mlco2_report.generate(0.56, 1.68)
+mlco2_report.generate(3,1.68)
