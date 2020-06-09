79692e4ec9fc2f8723b784e97467a567a92b173d
zziyaow <ziyaow36@gmail.com>
2020-06-09 16:42:11 -0400

    change bar chart and label position

--- energyusage/mlco2_report.py
+++ energyusage/mlco2_report.py
@@ -108,13 +108,16 @@ def comparison_graphs(comparison_values, location, emission, default_emissions, 
             drawing.add(label3)
         elif comparison_region == "Global":
             bc1 = gen_bar_graphs(default_emissions[:3], location, emission)
-            drawing.add(bc1)
-            label1 = Label()
-            label1.setText("Global (excluding Europe and US)")
-            bc1.x = 50
-            bc1.y = -160
+            offset = -257
+            bc1.x = 190 + offset
             bc1.height = 200
             bc1.width = 300
+            drawing.add(bc1)
+
+            label_offset = offset + 80
+            label1 = Label()
+            label1.setText("Global (excluding Europe and US)")
+            label1.x, label1.y = 185 + label_offset, -160
             label1.fontName = "Times-Bold"
             drawing.add(label1)
 
