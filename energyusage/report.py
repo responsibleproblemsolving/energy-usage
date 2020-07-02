d63c04dc7f960dfceb692f0d62713a2dbe929959
Ziyao Wang <ziyaow36@gmail.com>
2020-07-02 15:50:56 -0400

    fix y axis max value

--- energyusage/report.py
+++ energyusage/report.py
@@ -208,7 +208,8 @@ def gen_bar_graphs(comparison_values, location, emission):
     bc.strokeColor = colors.black
     bc.valueAxis.valueMin = 0
     bc.valueAxis.valueMax = data[0][-1] + data[0][-1] *.1
-    bc.valueAxis.valueStep = float(format(bc.valueAxis.valueMax, '.1g')) / 3
+    y_step = float(format(data[0][-1], '.1g')) / 3
+    bc.valueAxis.valueStep = float(format(y_step, '.1g'))
     bc.categoryAxis.labels.boxAnchor = 'ne'
     bc.categoryAxis.labels.dx = 8
     bc.categoryAxis.labels.dy = -2
